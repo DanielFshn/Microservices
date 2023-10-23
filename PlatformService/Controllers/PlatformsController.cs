34cc25da3b8199631f7be08f95715afa19ad6df7
Daniel Rakipllari <daniel.rakipllari@ikubinfo.al>
2023-10-23 23:33:05 +0200

    Add project files.

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo _platformRepo;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(IPlatformRepo platformRepo, IMapper mapper) =>
+        (_platformRepo, _mapper) = (platformRepo, mapper);
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        var platforms = _platformRepo.GetAllPlatforms();
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+
+    [HttpGet("{id}", Name = "GetPlatformById")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id)
+    {
+        var platformMayBe = _platformRepo.GetPlatformById(id);
+
+        if (platformMayBe != null)
+            return Ok(_mapper.Map<PlatformReadDto>(platformMayBe));
+        return NotFound();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PlatformReadDto>> CreatePlatform(PlatformCreateDto request)
+    {
+        var platformModel = _mapper.Map<Platform>(request);
+        _platformRepo.CreatePlatform(platformModel);
+        _platformRepo.SaveChanges();
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+        //try
+        //{
+        //    await _commandDataClient.SendPlatformToCommand(platformReadDto);
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine($"--> Could not sent synchronously: {e.Message}");
+        //}
+
+
+        //try
+        //{
+        //    var platformPublishedDto = _mapper.Map<PlatformPublishedDto>(platformReadDto);
+        //    platformPublishedDto.Event = "Platform_Published";
+        //    _messageBusClient.PublishNewPlatform(platformPublishedDto);
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine($"--> Could not sent asynchronously: {e.Message}");
+        //}
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { id = platformReadDto.Id }, platformReadDto);
+    }
+}
---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder app, bool isProduction = false)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            SeedDate(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProduction);
+        }
+    }
+
+    private static void SeedDate(AppDbContext? context, bool isProduction = false)
+    {
+        if (isProduction)
+        {
+            Console.WriteLine("--> Running migrations");
+            try
+            {
+                context.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"--> Could not run migrations {e.Message}");
+                throw;
+            }
+
+        }
+
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine("---> Seeding data");
+            context.Platforms.AddRange(new[]
+            {
+                new Platform()
+                {
+                    Cost = "Free",
+                    Name = "Dot Net",
+                    Publisher = "Microsoft"
+                },
+                new Platform()
+                {
+                    Cost = "Free",
+                    Name = "SQL Server Express",
+                    Publisher = "Microsoft"
+                },
+                new Platform()
+                {
+                    Cost = "Free",
+                    Name = "Kubernetes",
+                    Publisher = "Cloud Native Computer Foundation"
+                }
+            });
+
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("---> We already have data");
+        }
+
+    }
+}
